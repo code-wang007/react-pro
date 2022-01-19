728ea994d982e9746cc8f6feac1e080ceae0fdb9
code-wang007 <394915062@qq.com.com>
2022-01-19 23:52:19 +0800

    全局配置eslint和typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './style.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="root-content">
+        react root content change
+        <span>haha</span>
+      </div>
+    );
+  }
+}
+
+export default App;
